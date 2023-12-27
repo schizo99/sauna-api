040ac9b84583f2184957fe60c9a5e9de64ed063c
schizo <schizo99@gmail.com>
2023-12-27 12:23:31 +0100

    Do graceful shutdown

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const app = require('./app')
 
 const port = process.env.PORT || '3000'; app.listen(port);
 
+function shutdownGracefully() {
+    console.log("Server doing graceful shutdown");
+    app.server.close();
+}
 console.log(`Listening on port ${port}`);
+
+process.on("SIGINT", shutdownGracefully);
+process.on("SIGTERM", shutdownGracefully);
